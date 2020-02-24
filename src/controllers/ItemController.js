0ea31bc73d508e7eed82229cfc4593fe7fd8cdc0
JUNIOR <junior@iMac-de-JUNIOR.local>
2020-02-24 16:17:13 -0300

    feat: backend complete

--- src/controllers/ItemController.js
+++ src/controllers/ItemController.js
@@ -1,13 +1,31 @@
+import sharp from 'sharp';
+import path from 'path';
+import fs from 'fs';
+
 import Item from '../models/Item';
 
 class ItemController {
   async index(req, res) {
-    return res.send('deu certo');
+    const itens = await Item.find().sort('-createdAt');
+
+    return res.json(itens);
   }
 
   async store(req, res) {
     const { name, quantity, unit, price, cart } = req.body;
     const { filename: image } = req.file;
+
+    const [nameFile] = image.split('.');
+    const filename = `${nameFile}.jpg`;
+
+    // Redimensiona a imagem
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, 'resized', filename));
+
+    // Apaga a imagem original, deixando apenas a redimensionada
+    fs.unlinkSync(req.file.path);
 
     const item = await Item.create({
       name,
@@ -15,8 +33,10 @@ class ItemController {
       unit,
       price,
       cart,
-      image,
+      filename,
     });
+
+    req.io.emit('item', item);
 
     return res.json(item);
   }
--- src/server.js
+++ src/server.js
@@ -1,18 +1,36 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(
-  'mongodb+srv://applist:applist@cluster0-0laro.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://applist:applist@cluster0-0laro.mongodb.net/list?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
+);
+
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
