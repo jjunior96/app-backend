d8571b6d8c7ed484f67fada77eec47760b7b41ac
JUNIOR <junior@iMac-de-JUNIOR.local>
2020-02-24 15:04:40 -0300

    feat: route create from item

--- src/controllers/ItemController.js
+++ src/controllers/ItemController.js
@@ -1,7 +1,25 @@
 import Item from '../models/Item';
 
 class ItemController {
-  async index(req, res) {}
+  async index(req, res) {
+    return res.send('deu certo');
+  }
+
+  async store(req, res) {
+    const { name, quantity, unit, price, cart } = req.body;
+    const { filename: image } = req.file;
+
+    const item = await Item.create({
+      name,
+      quantity,
+      unit,
+      price,
+      cart,
+      image,
+    });
+
+    return res.json(item);
+  }
 }
 
 export default new ItemController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
+import ItemController from './controllers/ItemController';
 
 const routes = new Router();
+const upload = multer(uploadConfig);
 
-routes.get('/', (req, res) => {
-  return res.send('Testando');
-});
+routes.get('/itens', ItemController.index);
+routes.post('/itens', upload.single('image'), ItemController.store);
 
 export default routes;
